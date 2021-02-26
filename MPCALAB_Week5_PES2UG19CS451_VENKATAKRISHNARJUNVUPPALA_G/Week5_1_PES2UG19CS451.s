c95d842459676559edd9b46ebf2ee8c12ca54f3c
Arjun Vuppala <arjunv2001@gmail.com>
2021-02-26 06:26:49 +0530

    third

---
+++ MPCALAB_Week5_PES2UG19CS451_VENKATAKRISHNARJUNVUPPALA_G/Week5_1_PES2UG19CS451.s
@@ -0,0 +1,11 @@
+.DATA
+ A:   .ASCIZ   "HELLO WORLD"
+.text
+	LDR R1, =A
+	LOOP:     
+		LDRB  R0, [R1], #1
+                CMP R0, #0
+                SWINE 0x00            
+	        BNE LOOP 
+                SWI  0x11                  
+ 
